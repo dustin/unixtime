710c9f33779d28705212fdbfca942929f42730a0
Dustin Sallings <dustin@spy.net>
2025-01-17 19:15:53 -1000

    Beginning

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My Zig Project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      eachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      packages = eachSystem (system: let pkgs = import nixpkgs { inherit system; }; in {
+        default = pkgs.stdenv.mkDerivation {
+          name = "my-zig-project";
+          src = ./.;
+          buildInputs = [ pkgs.zig ];
+        };
+      });
+
+      devShells = eachSystem (system: let pkgs = import nixpkgs { inherit system; }; in {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.zig ];
+        };
+      });
+    };
+}
