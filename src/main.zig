6fd589a315ba219a325e2e657c5f1db67ec30505
Dustin Sallings <dustin@spy.net>
2025-01-18 09:33:24 -1000

    Added some tests

--- src/main.zig
+++ src/main.zig
@@ -27,7 +27,36 @@ fn parseTimestamp(bytes: []const u8) !i64 {
 
     return val;
 }
-fn formatUnixTimeC(timestamp: i64) ![]const u8 {
+
+test parseTimestamp {
+    const Expected = union(enum) {
+        value: i64,
+        err: anyerror,
+    };
+
+    const testCases = [_]struct {
+        input: []const u8,
+        expected: Expected,
+    }{
+        .{ .input = "1234", .expected = .{ .value = 1234 } },
+        .{ .input = "1234.5", .expected = .{ .value = 1234 } },
+        .{ .input = "abc", .expected = .{ .value = 0 } },
+    };
+
+    for (testCases) |testCase| {
+        const actualOrErr = parseTimestamp(testCase.input);
+        switch (testCase.expected) {
+            .value => |expVal| {
+                try std.testing.expectEqual(expVal, actualOrErr);
+            },
+            .err => |expErr| {
+                try std.testing.expectError(expErr, actualOrErr);
+            },
+        }
+    }
+}
+
+fn format(timestamp: i64) ![]const u8 {
     var t: c.time_t = @intCast(timestamp);
 
     const cstr = c.ctime(&t);
@@ -35,19 +64,22 @@ fn formatUnixTimeC(timestamp: i64) ![]const u8 {
 
     return std.mem.span(cstr);
 }
+
+test format {
+    try std.testing.expectEqualSlices(u8, "Sat Jan 18 09:32:28 2025\n", try format(1737228748));
+}
+
 pub fn printTS(t: i64) !void {
-    try std.io.getStdOut().writer().print("{d} {s}", .{ t, try formatUnixTimeC(t) });
+    try std.io.getStdOut().writer().print("{d} {s}", .{ t, try format(t) });
 }
 
 pub fn main() !void {
-    // Prepare a general-purpose allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         _ = gpa.deinit();
     }
     const allocator = gpa.allocator();
 
-    // Get CLI args
     var args = std.process.argsAlloc(allocator) catch return;
     defer std.process.argsFree(allocator, args);
 
